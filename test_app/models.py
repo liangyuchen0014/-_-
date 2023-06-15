c5e835e8257e5d9043446e804bb2c8a946290fc4
19374047 <19374047@buaa.edu.cn>
2023-06-15 16:58:13 +0800

    修改 models.py 添加client客户表

--- test_app/models.py
+++ test_app/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     description = models.TextField(blank=True, null=True)
     state = models.IntegerField(default=0)
@@ -17,3 +17,12 @@ class User(AbstractUser):
             'state': self.state,
             'head_url': self.head_url
         }
+
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)  # id
+    legal_person = models.CharField(max_length=255)  # 法人名称
+    company = models.CharField(max_length=255)  # 公司名称
+    contact_name = models.CharField(max_length=255)  # 联系人姓名
+    phone = models.IntegerField(default=0)  # 电话
+
