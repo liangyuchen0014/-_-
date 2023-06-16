d221969d725f32678e2ffa9fce836239b5c201c1
19374047 <19374047@buaa.edu.cn>
2023-06-16 15:21:03 +0800

    修改 models.py 修改User表

--- test_app/models.py
+++ test_app/models.py
@@ -6,9 +6,9 @@ class User(AbstractUser):
     user_id = models.AutoField(primary_key=True)
     legal_person = models.CharField(max_length=255, null=True)  # 法人名称
     company = models.CharField(max_length=255, null=True)  # 公司名称
-    contact_name = models.CharField(max_length=255, null=True)  # 联系人姓名
+    # contact_name = models.CharField(max_length=255, null=True)  # 联系人姓名
     phone = models.CharField(max_length=255, null=True)  # 电话
-    name = models.CharField(max_length=255)  # 工作人员个人姓名
+    name = models.CharField(max_length=255, null=True)  # 工作人员个人姓名或客户联系人姓名
     description = models.TextField(blank=True, null=True)
     type = models.IntegerField(default=0)  # 0:客户  1:水  2:电  3:机械  4:管理人员
     is_available = models.IntegerField(default=0)  # 0:空闲  1:不空闲
