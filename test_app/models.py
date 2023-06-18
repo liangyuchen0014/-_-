f6566cb5f5987637c720a6fdd4d7acd85fedca6a
Faker <75046675@qq.com>
2023-06-18 13:12:20 +0800

    修改models.py 取消工单表部分外键

--- test_app/models.py
+++ test_app/models.py
@@ -33,13 +33,13 @@ class User(AbstractUser):
 
 class Payment(models.Model):
     id = models.AutoField(primary_key=True)
-    lease_id = models.ForeignKey('Lease', on_delete=models.DO_NOTHING)  # 租赁信息id
+    lease_id = models.ForeignKey('Lease', on_delete=models.DO_NOTHING, null=True)  # 租赁信息id
     time = models.CharField(max_length=255, null=True)
 
 
 class Visitor(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.DO_NOTHING)  # 客户id
+    user_id = models.ForeignKey('User', on_delete=models.DO_NOTHING, null=True)  # 客户id
     name = models.CharField(max_length=255, null=True)  # 访客人员姓名
     number = models.CharField(max_length=255, null=True)  # 身份证号码
     visit_time = models.CharField(max_length=255, null=True)  # 到访时间
@@ -60,8 +60,8 @@ class Room(models.Model):
 
 class Lease(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.DO_NOTHING)  # 客户id(外键)
-    room_id = models.ForeignKey('Room', on_delete=models.DO_NOTHING)  # 房间id(外键)
+    user_id = models.ForeignKey('User', on_delete=models.DO_NOTHING, null=True)  # 客户id(外键)
+    room_id = models.ForeignKey('Room', on_delete=models.DO_NOTHING, null=True)  # 房间id(外键)
     room_number = models.CharField(max_length=255, null=True)  # 房间号（四位，如1001）
     start_time = models.CharField(max_length=255, null=True)  # 起租时间
     end_time = models.CharField(max_length=255, null=True)  # 终止时间
@@ -74,17 +74,17 @@ class RepairForm(models.Model):
     type = models.IntegerField(default=0)  # 问题类型 1:水  2:电  3:机械  4:其他
     repair_time = models.CharField(max_length=255, null=True)  # 报修时间
     room_number = models.CharField(max_length=255, null=True)  # 房间号
-    room_id = models.ForeignKey('Room', on_delete=models.DO_NOTHING)  # 房间id(外键)
+    room_id = models.ForeignKey('Room', on_delete=models.DO_NOTHING, null=True)  # 房间id(外键)
     company_name = models.CharField(max_length=255, null=True)  # 报修公司名称
-    company_id = models.ForeignKey('User', on_delete=models.DO_NOTHING)  # 报修公司id(外键)
+    company_id = models.ForeignKey('User', on_delete=models.DO_NOTHING, null=True)  # 报修公司id(外键)
     contact_name = models.CharField(max_length=255, null=True)  # 报修联系人姓名
     contact_phone = models.CharField(max_length=255, null=True)  # 报修联系人电话
     maintain_time = models.CharField(max_length=255, null=True)  # 上门维修时间
     maintainer_name = models.CharField(max_length=255, null=True)  # 维修人员姓名
-    maintainer_id = models.ForeignKey('User', on_delete=models.DO_NOTHING)  # 维修人员id(外键)
+    maintainer_id = models.CharField(max_length=255, null=True)  # 维修人员id
     maintainer_phone = models.CharField(max_length=255, null=True)  # 维修人员电话
     feedback_time = models.CharField(max_length=255, null=True)  # 反馈时间
     solution = models.TextField(blank=True, null=True)  # 解决方法
     solve_time = models.CharField(max_length=255, null=True)  # 解决时间
     solver_name = models.CharField(max_length=255, null=True)  # 解决人员姓名
-    solver_id = models.ForeignKey('User', on_delete=models.DO_NOTHING)  # 解决人员id(外键)
+    solver_id = models.CharField(max_length=255, null=True)  # 解决人员id
