9b3986c6d22b18d5c6c495ad0503642a95b96e33
19374047 <19374047@buaa.edu.cn>
2023-06-19 16:03:49 +0800

    添加urls

--- test_app/urls.py
+++ test_app/urls.py
@@ -37,4 +37,6 @@ urlpatterns = [
     path('saveLeaseInfo', save_lease),
     path('deleteLeaseInfo', del_lease),
     path('getWorker', get_worker),
+    path('deleteClientInfo', deleteClientInfo),
+    path('addNewClient', addNewClient)
 ]
