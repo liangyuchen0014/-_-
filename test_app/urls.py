06edad564ba7d6358bff79c58a77ae38552d4ab7
ligseeker <2381279969@qq.com>
2023-06-26 21:38:31 +0800

    Revert 添加首页获取今日报修工单借口哦偶
    This reverts commit 40b2c9fe

--- test_app/urls.py
+++ test_app/urls.py
@@ -53,6 +53,5 @@ urlpatterns = [
     path('AddWorker', add_worker),
     path('getVisitorNumber', get_visitor_num),
     path('send_sms', send_sms),
-    path('getStatistics', get_total_num),
-    path('getTodayOrder', get_today_repair),
+    path('getStatistics', get_total_num)
 ]
